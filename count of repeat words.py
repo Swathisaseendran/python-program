a0e9271e1404272bf2cdcf7a604819e6ed749652
LENOVO <sasswathi18@gmail.com>
2021-03-04 22:56:48 -0500

    count the occurrences of 'a'

---
+++ count of repeat words.py
@@ -0,0 +1,5 @@
+#store a list of first names.count the occurrences of 'a' within the list.
+
+names=['arha','ayaan','niha','yuva','ziva']
+for i in names:
+    print("no:of occurances of 'a' in",i,"is",i.count('a'))
