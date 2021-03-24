d6fcfa20e3791ff8eca490d9aa2876de02c43ef9
Swathisaseendran <79155802+Swathisaseendran@users.noreply.github.com>
2021-03-24 14:58:10 +0530

    graphics co3

---
+++ main.py
@@ -0,0 +1,15 @@
+import graphics.circle as ci
+import graphics.rectangle as re
+import graphics.dgraphics.cuboid as cu
+import graphics.dgraphics.sphere as sp
+print("area of circle",ci.area(3))
+print("perimeter of circle",ci.perimeter(3))
+print("area of rectangle",re.area(3,2))
+print("perimeter of rectangle",re.perimeter(3,2))
+print("area of cuboid",cu.area(3,2,4))
+print("perimeter of cuboid",cu.perimeter(3,2,4))
+print("area of sphere",sp.area(3))
+print("perimeter of sphere",sp.perimeter(3))
+
+
+
